c1aaca860aac27d40bb592d5d81555de6255555a
Akash Devgan <akash.devgan@gmail.com>
2022-06-28 16:53:35 -0400

    Random enemy movement

--- dungeoncrawl/src/components.rs
+++ dungeoncrawl/src/components.rs
@@ -3,7 +3,7 @@ pub use crate::prelude::*;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
     pub color: ColorPair,
-    pub glyph: FontCharType
+    pub glyph: FontCharType,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -11,3 +11,6 @@ pub struct Player;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct MovingRandomly;
